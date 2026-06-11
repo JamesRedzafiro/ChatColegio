e4c6f24abf12c6ea91bb31a94a88c384aa6dd409
James_Red <james.pinas@gmail.com>
2026-06-11 00:02:16 -0500

    Spring boot + dependencias

---
+++ src/main/java/com/sistemasdistribuidos/chatcolegio/ChatcolegioApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasdistribuidos.chatcolegio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatcolegioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatcolegioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemasdistribuidos/chatcolegio/ChatcolegioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemasdistribuidos.chatcolegio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatcolegioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
